cf40e19485817d53ee1b915f9a04d9464ec7f835
Sathishboobalan <31285976+Sathishboobalan@users.noreply.github.com>
2018-02-14 22:52:42 +0530

    Create posneg.py

---
+++ posneg.py
@@ -0,0 +1,11 @@
+a=int(input())
+if a >=0:
+    if a == 0:
+        print ("zero")
+    else:
+        print("positive")
+elif (a<=0):
+    if a==0:
+        print ("zero")
+    else:
+        print("negative")
