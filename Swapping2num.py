e52623f24826bf760b7a0556555138d5c9fe3a84
Sathish Boobalan <31285976+Sathishboobalan@users.noreply.github.com>
2018-03-10 17:35:12 +0530

    Update Swapping2num.py

--- Swapping2num.py
+++ Swapping2num.py
@@ -1,7 +1,7 @@
 #swapping or Two  numberz
 a=int(input("enter the first element"))
 b=int(input("Enter the second element"))
-a=a+b
-b=a-b
-a=a-b
+a=a*b
+b=int(a/b)
+a=int(a/b)
 print(" a=",a,"b=",b)
