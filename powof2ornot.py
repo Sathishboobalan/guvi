3adb2ce860a18d539479e367523be1ddaf5fcee5
Sathish Boobalan <31285976+Sathishboobalan@users.noreply.github.com>
2018-03-12 21:05:08 +0530

    Update powof2ornot.py

--- powof2ornot.py
+++ powof2ornot.py
@@ -1,13 +1,16 @@
 #power of 2 or not
 def pow_of_two(n):
-    n=int(n/2)
-    if (n==2):
+    if(n==2):
         print("yes")
     else:
-        if n>2:
-            pow_of_two(n)
+        n=int(n/2)
+        if (n==2):
+            print("yes")
         else:
-            print("no")
+            if n>2:
+                pow_of_two(n)
+            else:
+                print("no")
 try:
     n=int(input("Enter the number"))
     pow_of_two(n)
