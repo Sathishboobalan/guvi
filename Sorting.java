961f4872dd59736aae0a9c94d3e8fe8411733b20
Sathish Boobalan <31285976+Sathishboobalan@users.noreply.github.com>
2018-03-18 21:56:32 +0530

    Create Sorting.java

---
+++ Sorting.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Sorting{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num;
+		num=scan.nextInt();
+		int [] arr = new int[num];
+		for (int i=0;i<num;i++) {
+			arr[i]=scan.nextInt();
+		}
+		scan.close();
+		for (int i=0;i<num;i++) {
+			for(int j=0;j<num;j++) {
+				if(arr[i]<arr[j]) {
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+		for (int i=0;i<num;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
